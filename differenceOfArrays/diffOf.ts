562b1448ab38583227a416371f4e95b418bd4ba2
Atharve Pandey <pandey.atharve@gmail.com>
2023-08-01 14:15:28 -0400

    added a basic background to the problem

---
+++ differenceOfArrays/diffOf.ts
@@ -0,0 +1,11 @@
+// Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+// Note: You can return the array with its elements in any order.
+
+function diffArray(arr1: any[], arr2: any[]) {
+    const newArr: any[] = [];
+    return newArr;
+  }
+  
+  diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]); //return an array [4], as it is the only element not present in both arrays given
+
+  
