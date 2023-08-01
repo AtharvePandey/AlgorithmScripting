3f93fda89479630c66d5e4c4483a8da6f1227afd
Atharve Pandey <pandey.atharve@gmail.com>
2023-08-01 13:07:49 -0400

    added a basic outline

--- sumAllNumsInRange/sumAll.ts
+++ sumAllNumsInRange/sumAll.ts
@@ -1,3 +1,14 @@
 // We'll pass you an array of two numbers. Return the sum of those two numbers plus the sum of all the numbers between them. The lowest number will not always come first.
 // For example, sumAll([4,1]) should return 10 because sum of all the numbers between 1 and 4 (both inclusive) is 10.
 
+//I will use a recursive helper function to aid my main function... 
+
+function sumAll(arr: number[]) {
+  
+}
+
+function recur(numOne: number, numTwo: number){
+
+}
+
+sumAll([1, 4]); //should return the same thing as sumAll([4,1]): 10 
