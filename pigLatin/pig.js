34208433ed7d259b32251f5aceca0a8eb5dcb3a8
Atharve Pandey <pandey.atharve@gmail.com>
2023-08-03 12:38:30 -0400

    wrote for edge cases, now time to debug and test

--- pigLatin/pig.js
+++ pigLatin/pig.js
@@ -2,7 +2,30 @@
 // - If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
 // - If a word begins with a vowel, just add way at the end.
 function translatePigLatin(str) {
-    var char = str.charAt(0);
-    return str.slice(1).concat(char + "ay");
+    if (['a', 'e', 'i', 'o', 'u'].includes(str.charAt(0))) {
+        return str.concat("way");
+    }
+    else {
+        var tempArr = stringToArr(str);
+        var j = 0;
+        while (!(['a', 'e', 'i', 'o', 'u'].includes(tempArr[j]))) {
+            if (j == tempArr.length) { //used to deal with words without vowels...
+                return str.concat("ay");
+            }
+            var tempChar = tempArr.shift();
+            tempArr.push(tempChar);
+            j++;
+        }
+        tempArr.push("a");
+        tempArr.push("y");
+        return tempArr.join("");
+    }
+}
+function stringToArr(str) {
+    var tempArr = [];
+    for (var i = 0; i < str.length; i++) {
+        tempArr[i] = str.charAt(i);
+    }
+    return tempArr;
 }
 console.log(translatePigLatin("consonant")); //should print onsonantcay to the console...
--- pigLatin/pig.ts
+++ pigLatin/pig.ts
@@ -2,9 +2,33 @@
 // - If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
 // - If a word begins with a vowel, just add way at the end.
 
-function translatePigLatin(str: String) {
-    let char = str.charAt(0); 
-    return str.slice(1).concat(char + "ay");
+function translatePigLatin(str: string) {
+  if (['a', 'e', 'i', 'o', 'u'].includes(str.charAt(0))) {
+    return str.concat("way");
+  } else {
+    let tempArr: string[] = stringToArr(str); 
+    let j = 0;
+    while (!(['a', 'e', 'i', 'o', 'u'].includes(tempArr[j]))) {
+      if(j == tempArr.length){ //used to deal with words without vowels...
+        return str.concat("ay");
+      }
+      let tempChar: string = tempArr.shift()!;
+      tempArr.push(tempChar);
+      j++;
+    }
+    tempArr.push("a");
+    tempArr.push("y");
+    return tempArr.join(""); 
   }
-  
-  console.log(translatePigLatin("consonant")); //should print onsonantcay to the console...
+}
+
+function stringToArr(str: String) {
+  let tempArr: string[] = [];
+  for (let i = 0; i < str.length; i++) {
+    tempArr[i] = str.charAt(i);
+  }
+  return tempArr; 
+
+}
+
+console.log(translatePigLatin("consonant")); //should print onsonantcay to the console...
