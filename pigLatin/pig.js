c82dec082a93a5a0b5e04e9e716bdb36231fb615
Atharve Pandey <pandey.atharve@gmail.com>
2023-08-02 20:37:54 -0400

    wrote part of the algorithm, now have to do the cases where we check for vowels or constonants...

--- pigLatin/pig.js
+++ pigLatin/pig.js
@@ -1,10 +1,8 @@
 // Pig Latin is a way of altering English Words. The rules are as follows:
 // - If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add ay to it.
 // - If a word begins with a vowel, just add way at the end.
-
-function translatePigLatin(str: String) {
-    let char = str.charAt(0); 
+function translatePigLatin(str) {
+    var char = str.charAt(0);
     return str.slice(1).concat(char + "ay");
-  }
-  
-  console.log(translatePigLatin("consonant")); //should print onsonantcay to the console...
+}
+console.log(translatePigLatin("consonant")); //should print onsonantcay to the console...
