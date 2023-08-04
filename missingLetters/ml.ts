42429080b86f18cf36e710744fbdaf3a65939161
Atharve Pandey <pandey.atharve@gmail.com>
2023-08-04 12:56:29 -0400

    added backbone for the method and also a helper function that converst given string into an array

---
+++ missingLetters/ml.ts
@@ -0,0 +1,14 @@
+// Find the missing letter in the passed letter range and return it.
+// If all letters are present in the range, return undefined.
+//i.e "abde" would return c since c is missing from the sequence...
+
+function fearNotLetter(str:string) {
+    return str;
+  }
+  
+  fearNotLetter("abce"); //return "d" since "d" is missing in the sequence...
+
+
+  function strToArr(str:string){
+
+  }
