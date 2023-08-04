2a413a6be4ae24c62b3991c9edb7c2bab7a272c6
Atharve Pandey <pandey.atharve@gmail.com>
2023-08-04 13:36:32 -0400

    added a basic algorithm for the problem, now will compile using tsc and then debug if required

--- missingLetters/ml.ts
+++ missingLetters/ml.ts
@@ -3,12 +3,25 @@
 //i.e "abde" would return c since c is missing from the sequence...
 
 function fearNotLetter(str:string) {
-    return str;
+    let strArr = strToArr(str); 
+    let j = 0;
+    for(let i = 1; i<strArr.length; i++){
+        if(strArr[i] !== ++strArr[j].toString()){
+            return strArr[j];
+        }else{
+            j++;
+        }
+    }
+    return undefined; 
   }
   
-  fearNotLetter("abce"); //return "d" since "d" is missing in the sequence...
+  console.log(fearNotLetter("abce")); //return "d" since "d" is missing in the sequence...
 
 
   function strToArr(str:string){
-
+    let arr:string[] = [];
+    for(let i = 0; i<str.length; i++){
+        arr.push(str.charAt(i));
+    }
+    return arr; 
   }
