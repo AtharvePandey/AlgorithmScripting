e2986e988aa8d38f436662910576ce28c51283f0
Atharve Pandey <pandey.atharve@gmail.com>
2023-08-04 13:47:52 -0400

    added basic backbone to this next algorithm

---
+++ sortedUnion/sorted.ts
@@ -0,0 +1,14 @@
+// Write a function that takes two or more arrays and returns a new array of unique values in the order of the 
+// original provided arrays.
+// In other words, all values present from all arrays should be included in their original order, but with no duplicates 
+// in the final array.
+// The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+// i.e [[1,2,3], [4,5,2,3,1,2,2]] should return [1,2,3,4,5]; we don't want to repeat numbers, and we want to add them in order
+// but not sorted
+
+function uniteUnique(arr:number[]) {
+    return arr;
+  }
+  
+  uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+  
