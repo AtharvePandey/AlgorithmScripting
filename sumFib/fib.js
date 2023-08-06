b17c91c37d6ed43ce5ec912e0c4e96df9e1195c3
Atharve Pandey <pandey.atharve@gmail.com>
2023-08-06 18:53:11 -0400

    added the actual method implementation, done with helper method..

---
+++ sumFib/fib.js
@@ -0,0 +1,27 @@
+// Given a positive integer num, return the sum of all odd Fibonacci numbers that are less than or equal to num.
+// The first two numbers in the Fibonacci sequence are 0 and 1. Every additional number in the sequence is the sum of the two previous numbers. 
+//The first seven numbers of the Fibonacci sequence are 0, 1, 1, 2, 3, 5 and 8.
+// For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
+function sumFibs(num) {
+    return num;
+}
+sumFibs(4);
+//putting fib seq into array and then using reduce in the sumFibs function to calculate the sum of all odd digit...
+function fibIntoArr(numsInArr) {
+    var retArr = [];
+    if (numsInArr === 1) { //first number in sequence
+        return retArr.push(0);
+    }
+    else if (numsInArr === 2) { //first two numbers in sequence... 
+        return retArr.push(0, 1);
+    }
+    else { //first n numbers in the sequence...
+        retArr.push(0);
+        retArr.push(1);
+        for (var index = 2; index < numsInArr; index++) {
+            retArr.push(retArr[index - 2] + retArr[index - 1]);
+        }
+        return retArr;
+    }
+}
+console.log(fibIntoArr(20)); //first 20 terms of the sequence... 
--- sumFib/fib.ts
+++ sumFib/fib.ts
@@ -3,10 +3,10 @@
 //The first seven numbers of the Fibonacci sequence are 0, 1, 1, 2, 3, 5 and 8.
 // For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than or equal to 10 are 1, 1, 3, and 5.
 
-
-
 function sumFibs(num:number) {
-    return num;
+    return fibIntoArr(num).reduce((sum:number, currNum:number):number => {
+        return sum += currNum % 2 !== 0 ?currNum < num ? num : 0 : 0; //add this number to the sum (initialized to zero) iff it is odd and less than num passed in...
+    }, 0);
   }
   
   sumFibs(4);
@@ -15,12 +15,14 @@ function sumFibs(num:number) {
   //putting fib seq into array and then using reduce in the sumFibs function to calculate the sum of all odd digit...
 
 
-  function fibIntoArr(numsInArr:number){ //takes in length the array should be...
+  function fibIntoArr(numsInArr:number):number[]{ //takes in length the array should be...
     let retArr:number[] = []; 
     if(numsInArr === 1){ //first number in sequence
-        return retArr.push(0);
+        retArr.push(0);
+        return retArr;
     }else if(numsInArr === 2){ //first two numbers in sequence... 
-        return retArr.push(0, 1); 
+        retArr.push(0, 1);
+        return retArr; 
     }else{ //first n numbers in the sequence...
         retArr.push(0);
         retArr.push(1);
