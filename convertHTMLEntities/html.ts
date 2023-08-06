71c87104b2394744ca08ad16929e223dc86914ae
Atharve Pandey <pandey.atharve@gmail.com>
2023-08-06 13:12:59 -0400

    finished with this algorithm, now adding a js file

--- convertHTMLEntities/html.ts
+++ convertHTMLEntities/html.ts
@@ -1,7 +1,7 @@
 // Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
 // i.e convertHTML("Dolce & Gabbana") should return the string Dolce &amp; Gabbana
 
-function convertHTML(str) {
+function convertHTML(str:string) {
     let obj = {
     '&': '&amp;',
     '<': '&lt;',
@@ -9,6 +9,8 @@ function convertHTML(str) {
     '"': '&quot;',
     "'": '&apos;'
     }
+    return str.replace(/[&<>"']/g,char => obj[char]); //basically the object defined above will act as a key to replace with some value;
+    //the replace method uses regex to find instances of symbols in the array provided and then will replace that cahr with the obj.char value...
   }
   
-  convertHTML("Dolce & Gabbana");ll
+  console.log(convertHTML("Dolce & Gabbana")); //should return Dolce &amp Gabbana...
