b0dc1ee32eff70ad8550215082fc28d398636b01
Atharve Pandey <pandey.atharve@gmail.com>
2023-08-05 22:41:58 -0400

    added backbone

--- convertHTMLEntities/html.ts
+++ convertHTMLEntities/html.ts
@@ -1,2 +1,8 @@
 // Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
 // i.e convertHTML("Dolce & Gabbana") should return the string Dolce &amp; Gabbana
+
+function convertHTML(str) {
+    return str;
+  }
+  
+  convertHTML("Dolce & Gabbana");ll
