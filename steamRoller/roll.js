966ca83b8229d7bc7deaeacd8314d60350ec0e92
Atharve Pandey <pandey.atharve@gmail.com>
2023-08-07 15:02:28 -0400

    done

---
+++ steamRoller/roll.js
@@ -0,0 +1,19 @@
+// Flatten a nested array. You must account for varying levels of nesting.
+// don't use methods alr provided and global variables.
+// im going to use a recursive approach with a helper function that does the recursion...
+function steamrollArray(arr) {
+    var retArr = [];
+    function recurHelper(arr) {
+        for (var i = 0; i < arr.length; i++) {
+            if (Array.isArray(arr[i])) {
+                recurHelper(arr[i]);
+            }
+            else {
+                retArr.push(arr[i]);
+            }
+        }
+    }
+    recurHelper(arr);
+    return retArr;
+}
+console.log(steamrollArray([1, [2], [3, [[4]]]])); //should return [1,2,3,4]
--- steamRoller/roll.ts
+++ steamRoller/roll.ts
@@ -1,8 +1,21 @@
 // Flatten a nested array. You must account for varying levels of nesting.
 // don't use methods alr provided and global variables.
 
-function steamrollArray(arr) {
-    return arr;
+// im going to use a recursive approach with a helper function that does the recursion...
+
+function steamrollArray(arr:any[]) {
+    let retArr:any[] = []
+    function recurHelper(arr:any[]){
+        for(let i = 0; i<arr.length; i++){
+          if(Array.isArray(arr[i])){
+            recurHelper(arr[i]);
+          }else{
+            retArr.push(arr[i])
+          }
+        }
+    }
+    recurHelper(arr);
+    return retArr;
   }
   
-  steamrollArray([1, [2], [3, [[4]]]]); //should return [1,2,3,4]
+  console.log(steamrollArray([1, [2], [3, [[4]]]])); //should return [1,2,3,4]
