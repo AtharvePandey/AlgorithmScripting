e1c7598c6414f9c6e534d57e972872b411054870
Atharve Pandey <pandey.atharve@gmail.com>
2023-08-07 13:59:05 -0400

    added backbone

---
+++ dropIt/drop.ts
@@ -0,0 +1,11 @@
+// Given the array arr, iterate through and remove each element starting from the first element (the 0 index) 
+// until the function func returns true when the iterated element is passed through it.
+// Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+// NOTE DO NOT USE FILTER BECAUSE WE WANT TO REMOVE UNTILE FUNCTION RETURNS TRUE, NOT REMOVE ALL ELEMENTS 
+// FOR WHICH THE FUNCTION RETURNS TRUE JUST UNTIL... 
+
+function dropElements(arr: number[], func: (any)) {
+    return arr; 
+  }
+  
+  dropElements([1, 2, 3], function(n) {return n < 3; });
