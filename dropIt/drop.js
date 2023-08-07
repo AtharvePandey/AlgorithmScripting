c880026ca880a2e022070abbf813eb7f6f31e5c3
Atharve Pandey <pandey.atharve@gmail.com>
2023-08-07 14:07:08 -0400

    done, now debugging

--- dropIt/drop.js
+++ dropIt/drop.js
@@ -3,14 +3,12 @@
 // Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
 // NOTE DO NOT USE FILTER BECAUSE WE WANT TO REMOVE UNTILE FUNCTION RETURNS TRUE, NOT REMOVE ALL ELEMENTS 
 // FOR WHICH THE FUNCTION RETURNS TRUE JUST UNTIL... 
-
-function dropElements(arr: number[], func: (any)) {
-    let i = 0;
-     while(func(arr[i])){
-        arr.shift(); 
+function dropElements(arr, func) {
+    var i = 0;
+    while (func(arr[i])) {
+        arr.shift();
         i++;
-     }
-     return arr; 
-  }
-  
-  console.log(dropElements([1, 2, 3], function(n) {return n < 3; }));
+    }
+    return arr;
+}
+console.log(dropElements([1, 2, 3], function (n) { return n < 3; }));
