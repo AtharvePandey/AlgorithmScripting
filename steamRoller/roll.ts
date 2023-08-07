2247dd6cfc0077240b771a4e449bf104b97baa9a
Atharve Pandey <pandey.atharve@gmail.com>
2023-08-07 14:39:59 -0400

    added backbone

---
+++ steamRoller/roll.ts
@@ -0,0 +1,8 @@
+// Flatten a nested array. You must account for varying levels of nesting.
+// don't use methods alr provided and global variables.
+
+function steamrollArray(arr) {
+    return arr;
+  }
+  
+  steamrollArray([1, [2], [3, [[4]]]]); //should return [1,2,3,4]
