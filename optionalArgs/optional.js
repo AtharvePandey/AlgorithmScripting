7952db216ebdb598d44cb9cb00c360fd846b06e2
Atharve Pandey <pandey.atharve@gmail.com>
2023-08-07 21:53:25 -0400

    debug time lol

--- optionalArgs/optional.js
+++ optionalArgs/optional.js
@@ -4,28 +4,26 @@
 // var sumTwoAnd = addTogether(2);
 // sumTwoAnd(3) returns 5.
 // If either argument isn't a valid number, return undefined.
-
 //use the arguments global variable to acccss the args... 
-
-
 function addTogether() {
     if (arguments.length == 1) { //case where we need to return a function else, undefined...
-        let input = arguments[0];
-        if (typeof input == "number") { //case where we return a closure...
-            return (num: number) => input + num;
-        } else {
+        var input_1 = arguments[0];
+        if (typeof input_1 == "number") { //case where we return a closure...
+            return function (num) { return input_1 + num; };
+        }
+        else {
             return undefined;
         }
-    } else { //case where we have two numbers
-        let arg1 = arguments[0];
-        let arg2 = arguments[1];
+    }
+    else { //case where we have two numbers
+        var arg1 = arguments[0];
+        var arg2 = arguments[1];
         if (typeof arg1 == "number" && typeof arg2 == "number") {
             return arg1 + arg2;
-        } else {
+        }
+        else {
             return undefined;
         }
-
     }
 }
-
 console.log(addTogether(2, 3)); //should return 5
