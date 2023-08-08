ac67757309ae25a4a0a6f7a622ce6a0086303146
Atharve Pandey <pandey.atharve@gmail.com>
2023-08-07 22:06:47 -0400

    done now adding a debug file

--- makePerson/make.ts
+++ makePerson/make.ts
@@ -6,11 +6,27 @@
 // setLastName(last)
 // setFullName(first, last)
 // Run the tests to see the expected output for each method. These methods must be the only 
-//available means of interacting with the object. Each test will declare a new Person instance as new Person('Bob', 'Ross').
+// available means of interacting with the object. Each test will declare a new Person instance as new Person('Bob', 'Ross').
 
-const Person = function(first, last) {
-    this.getFullName = function() {
-      return "";
+const Person = function (first: string, last: string) {
+    this.getFullName = function () {
+        return first + last;
     };
-    return "";
-  };
+    this.getFirstName = () => {
+        return first;
+    }
+    this.getLastName = () => {
+        return last;
+    }
+    this.setFirstName = (name: string) => {
+        this.first = name;
+    }
+    this.setLastName = (name: string) => {
+        this.last = name;
+    }
+    this.setFullName = (fn: string, ln: string) => {
+        this.first = fn;
+        this.last = ln;
+    }
+    return this;
+};
